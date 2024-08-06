8e5c0367d6569306c926d2c943104fae65dcfdf7
Devlion59 <cheremnyhdn@gmail.com>
2024-08-06 23:32:18 +0500

    new project

--- src/main/java/org/aston/course/Main.java
+++ src/main/java/org/aston/course/Main.java
@@ -1,13 +1,12 @@
-package org.example;
+package org.aston.course;
+
 
 /**
- * Hello world!
- *
+ * New project
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class Main {
+    public static void main( String[] args ) {
+
     }
 }
