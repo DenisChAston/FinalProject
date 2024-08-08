e7fafac7e4284228e0a8235bbea87328a28b8530
PatriKey <dolininaleksei@gmail.com>
2024-08-08 13:26:46 +0400

    Chenge class Bus with pattern Builder

--- src/main/java/org/aston/course/datasource/Bus.java
+++ src/main/java/org/aston/course/datasource/Bus.java
@@ -1,27 +1,25 @@
-package org.aston.course;
+package org.aston.course.datasource;
 //класс автобус
 public class Bus implements Comparable<Bus> {
-    private int number; //номер
+    private String number; //номер
     private String model; //модель
     private int mileage; //пробег
+
     //конструктор без параметров
     Bus () {
-        this.number = 0;
+        this.number = "x000xx000";
         this.model = "unknown";
         this.mileage = 0;
     }
     //конструктор с параметрами
-    Bus (int num, String mod, int mil) {
+    Bus (String num, String mod, int mil) {
         this.number = num; //номер
         this.model = mod;  //модель
         this.mileage = mil; //пробег
     }
-    //переопределение метода cравнения, сравнение по номеру)
-    public int compareTo (Bus other) { //возвращает отрицательное число, если первый меньше второго
-        return Integer.compare(number, other.number); //ноль, если объекты равны //иначе - положительное число
-    }
+
     // далее идут геттеры и сеттеры
-    public void setNumber (int num) {
+    public void setNumber (String num) {
         this.number = num;
     }
 
@@ -33,7 +31,7 @@ public class Bus implements Comparable<Bus> {
         this.mileage = mil;
     }
 
-    public int getNumber () {
+    public String getNumber () {
         return this.number;
     }
 
@@ -44,5 +42,77 @@ public class Bus implements Comparable<Bus> {
     public int getMileage () {
         return this.mileage;
     }
+    //конец геттеров и сеттеров
 
+    //переопределение метода cравнения, сравнение по номеру)
+    @Override //возвращает отрицательное число, если первый меньше второго
+    public int compareTo(Bus other) { //ноль, если объекты равны, иначе - положительное число
+        return this.number.toLowerCase().compareTo(other.number.toLowerCase()); //сравнение по номеру
+    } //перед сравнением приводим номера к нижнему регистру
+
+    //реализация паттерна Builder
+    public static class Builder {
+        private Bus newBus;
+
+        public Builder() {
+            newBus = new Bus();
+        }
+
+        public Builder withNumber(String num) {
+            newBus.number = num;
+            return this;
+        }
+
+        public Builder withModel(String mod) {
+            newBus.model = mod;
+            return this;
+        }
+
+        public Builder withMileage(int mil) {
+            newBus.mileage = mil;
+            return this;
+        }
+
+        public Bus build() {
+            return newBus;
+        }
+    }
+
+//проверка класса
+
+    public static void printClass(Bus B) {
+        System.out.print("Номер: " + B.number);
+        System.out.print(" Модель: " + B.model);
+        System.out.println(" Пробег: " + B.mileage);
+    }
+
+    public static void comparePair(Bus B1, Bus B2) {
+        int comp = B1.compareTo(B2);
+        if (comp == 0) {
+            System.out.println(B1.number + " и " + B2.number + " равны.");
+        }
+        else if (comp < 0) {
+            System.out.println(B1.number + " меньше " + B2.number);
+        }
+        else  System.out.println(B1.number + " больше " + B2.number);
+    }
+
+    public static void main(String[] args) {
+        Bus myBus1 = new Bus.Builder()
+                .withNumber("a007aa007")
+                .withModel("Ikarus")
+                .withMileage(32000)
+                .build();
+        Bus myBus2 = new Bus.Builder()
+                .withNumber("x001xx001")
+                .withModel("Luidor")
+                .withMileage(50000)
+                .build();
+        printClass(myBus1);
+        printClass(myBus2);
+        comparePair(myBus1, myBus2);
+        comparePair(myBus2, myBus1);
+        comparePair(myBus1, myBus1);
+        comparePair(myBus2, myBus2);
+    }
 }
