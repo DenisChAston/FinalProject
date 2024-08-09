27435a817ea8d50245cb2d80ca9d2c8939bb5184
Dionork <rodya.ru@inbox.ru>
2024-08-09 18:39:51 +0700

    2 Classloader (file and console)

--- src/main/java/org/aston/course/application/usecase/strategies/ConsoleLoadStrategyImpl.java
+++ src/main/java/org/aston/course/application/usecase/strategies/ConsoleLoadStrategyImpl.java
@@ -6,12 +6,24 @@ import org.aston.course.domain.model.business.EntityCreator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ConsoleLoadStrategyImpl implements LoadStrategy {
 
     @Override
     public <T extends SomeEntity> void load(List<? super T> list, EntityCreator<T> creator) {
-        T temp = creator.create("A001AA159", "MAZ", "1000");
-        list.add(temp);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество объектов в базе данных:");
+        for (int i = scanner.nextInt(); i > 0; i--) {
+            System.out.println("Введите первый параметр объекта");
+            String f = scanner.next();
+            System.out.println("Введите второй параметр объекта");
+            String s = scanner.next();
+            System.out.println("Введите третий параметр объекта");
+            String t = scanner.next();
+            T temp = creator.create(f, s, t);
+            list.add(temp);
+
+        }
     }
 }
--- src/main/java/org/aston/course/application/usecase/strategies/FileLoadStrategyImpl.java
+++ src/main/java/org/aston/course/application/usecase/strategies/FileLoadStrategyImpl.java
@@ -4,13 +4,41 @@ import org.aston.course.domain.model.SomeEntity;
 import org.aston.course.domain.model.application.LoadStrategy;
 import org.aston.course.domain.model.business.EntityCreator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileLoadStrategyImpl implements LoadStrategy {
 
     @Override
     public <T extends SomeEntity> void load(List<? super T> list, EntityCreator<T> creator) {
-        T temp = creator.create("UserName", "PASSW", "d@gmail.com");
-        list.add(temp);
+        System.out.println("Введите полный путь к файлу и его НАЗВАНИЕ.txt");
+        Scanner scanner = new Scanner(System.in);
+        String file = scanner.nextLine();
+        FileReader fr;
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedReader br = new BufferedReader(fr);
+        while (true) {
+            try {
+                if (!br.ready()) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String lineName[];
+            try {
+                lineName = br.readLine().split(" ");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            T temp = creator.create(lineName[0], lineName[1], lineName[2]);
+            list.add(temp);
+        }
     }
 }
