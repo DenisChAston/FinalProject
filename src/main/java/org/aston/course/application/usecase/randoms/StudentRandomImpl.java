d37ea19b9868a8f34ec5053b12672a7ce11328ef
Dionork <rodya.ru@inbox.ru>
2024-08-10 23:27:44 +0700

    Сделал класс СтудентРандом

--- src/main/java/org/aston/course/application/usecase/randoms/StudentRandomImpl.java
+++ src/main/java/org/aston/course/application/usecase/randoms/StudentRandomImpl.java
@@ -3,10 +3,24 @@ import org.aston.course.application.datasource.Student;
 import org.aston.course.domain.business.EntityCreator;
 import org.aston.course.domain.application.GeneratorStrategy;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class StudentRandomImpl implements GeneratorStrategy<Student> {
     @Override
     public Student generator(EntityCreator<Student> creator) {
-        Student temp = creator.create("1","2","3");
+        String fp = String.valueOf(groupNumberGen());
+        String sp = String.valueOf(averegeScore());
+        String tp = String.valueOf(groupNumberGen());
+        Student temp = creator.create(fp,sp,tp);
         return temp;
+    }
+    private int groupNumberGen (){
+
+        int randomNum = ThreadLocalRandom.current().nextInt(1000, 9998 + 1);
+        return randomNum;
+    }
+    private double averegeScore(){
+        double randomNum = ThreadLocalRandom.current().nextDouble(1, 4 + 1);
+        return randomNum;
     }
 }
--- src/main/java/org/aston/course/application/usecase/strategies/RandomLoadStrategyImpl.java
+++ src/main/java/org/aston/course/application/usecase/strategies/RandomLoadStrategyImpl.java
@@ -1,7 +1,5 @@
 package org.aston.course.application.usecase.strategies;
 
-import org.aston.course.application.datasource.Bus;
-import org.aston.course.application.datasource.Student;
 import org.aston.course.application.usecase.creators.BusCreatorImpl;
 import org.aston.course.application.usecase.creators.StudentCreatorImpl;
 import org.aston.course.application.usecase.creators.UserCreatorImpl;
@@ -12,21 +10,12 @@ import org.aston.course.domain.application.GeneratorStrategy;
 import org.aston.course.domain.model.SomeEntity;
 import org.aston.course.domain.application.LoadStrategy;
 import org.aston.course.domain.business.EntityCreator;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 public class RandomLoadStrategyImpl implements LoadStrategy, GeneratorStrategy {
 
     @Override
     public <T extends SomeEntity> void load(List<? super T> list, EntityCreator<T> creator) {
-//        Map<String, GeneratorStrategy> createStrategyMap;
-//        createStrategyMap = new HashMap<>();
-//        createStrategyMap.put("1", new BusRandomImpl());
-//        createStrategyMap.put("2", new StudentRandomImpl());
-//        createStrategyMap.put("3", new UserRandomImpl());
         T temp = (T) generator(creator);
         list.add(temp);
     }
